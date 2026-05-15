3d0c555057e57b456520d7edab774404f8e665bc
Kephira <arielleahlonkoba@gmail.com>
2026-05-15 09:55:18 +0000

    en constructio n 3

--- src/composant/Hero.jsx
+++ src/composant/Hero.jsx
@@ -127,15 +127,16 @@ export default function Hero() {
         {/* Bouton Global */}
           <div className=" flex justify-center items-center bg-white ">
              
-        <Link  to="/boutique" className='mt-10  mb-5'
+        <Link  to="/boutique" className='group relative mt-10  mb-5'
          >
           
-               <div className='w-65 h-0.5 bg-amber-700'></div>
-        <div className='bg-amber-500 w-59 h-15 border-l-2 border-amber-500 '>
+               <div className='absolute  w-65 h-0.5 bg-amber-700'></div>
+        <div className='bg-amber-700 w-59 h-15 border-l-2 border-amber-700 '>
   
-           <div className='flex items-center font-alice text-2xl h-12 bg-amber-50 w-56 p-2 hover:w-60 hover:h-15 hover:border-2 border-amber-500'>
+           <div className=' flex items-center font-caveat text-2xl h-12 bg-amber-50 w-56 p-2 hover:w-60 hover:h-15  hover:border-2 border-amber-700 duration-100'>
           <ShoppingBag className='w-5 h-5'/>
           Visitez la Boutique
+          <span className="absolute bottom-4 left-3w-0 h-0.5 bg-amber-700 transition-all duration-300 group-hover:w-45"></span>
         </div>
         </div>
       </Link>
